12b7cd1fd76dca60394c5a2c3acfe40044a908db
Jeff Lowdermilk <jeffml@google.com>
2017-03-16 21:05:19 -0700

    make guestbook a tetris game
    super hacky for demo only

--- examples/guestbook-go/public/script.js
+++ examples/guestbook-go/public/script.js
@@ -1,27 +1,33 @@
+var entryContentElement = null;
+var entriesElement = null;
+
+var appendGuestbookEntries = function(data) {
+  entriesElement.empty();
+  $.each(data, function(key, val) {
+    entriesElement.append("<p>" + val + "</p>");
+  });
+}
+
+var handleSubmission = function(score) {
+  console.log("sanity")
+  //e.preventDefault();
+  var name = entryContentElement.val()
+  if (name.length <= 0) {
+    name = "anonymous";
+  }
+  entriesElement.append("<p>...</p>");
+  val = name + ": " + score;
+  console.log(val);
+  $.getJSON("rpush/guestbook/" + val, appendGuestbookEntries);
+}
+
 $(document).ready(function() {
   var headerTitleElement = $("#header h1");
-  var entriesElement = $("#guestbook-entries");
-  var formElement = $("#guestbook-form");
-  var submitElement = $("#guestbook-submit");
-  var entryContentElement = $("#guestbook-entry-content");
+  entriesElement = $("#hi-scores");
+  //var formElement = $("#name");
+  //var submitElement = $("#guestbook-submit");
+  entryContentElement = $("#guestbook-entry-content");
   var hostAddressElement = $("#guestbook-host-address");
-
-  var appendGuestbookEntries = function(data) {
-    entriesElement.empty();
-    $.each(data, function(key, val) {
-      entriesElement.append("<p>" + val + "</p>");
-    });
-  }
-
-  var handleSubmission = function(e) {
-    e.preventDefault();
-    var entryValue = entryContentElement.val()
-    if (entryValue.length > 0) {
-      entriesElement.append("<p>...</p>");
-      $.getJSON("rpush/guestbook/" + entryValue, appendGuestbookEntries);
-    }
-    return false;
-  }
 
   // colors = purple, blue, red, green, yellow
   var colors = ["#549", "#18d", "#d31", "#2a4", "#db1"];
@@ -29,11 +35,11 @@ $(document).ready(function() {
   (function setElementsColor(color) {
     headerTitleElement.css("color", color);
     entryContentElement.css("box-shadow", "inset 0 0 0 2px " + color);
-    submitElement.css("background-color", color);
+    //submitElement.css("background-color", color);
   })(randomColor);
 
-  submitElement.click(handleSubmission);
-  formElement.submit(handleSubmission);
+  //submitElement.click(handleSubmission);
+  //formElement.submit(handleSubmission);
   hostAddressElement.append(document.URL);
 
   // Poll every second.
